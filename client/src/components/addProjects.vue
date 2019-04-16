ce442ab423b486b78cdb11f7e756ac033a54b36c
mohammad <mohammad@mohammads-MacBook-Pro.local>
2019-04-17 07:47:27 +1000

    add the section in the add project component for adding the project details.

--- client/src/components/addProjects.vue
+++ client/src/components/addProjects.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
-    this is the add new project
-    <section></section>
+    <div>
+      <h3>Add new project</h3>
+      <div>
+        <input type="text" v-model="project.name">
+      </div>
+      <div>
+        <input type="text" v-model="project.status">
+      </div>
+      <button @click="addNewProjec()">Add Project</button>
+    </div>
   </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from "vue";
+import { mapActions, mapGetters } from "vuex";
+import { IProject } from "../interface/project";
+import projects from "../store/modules/projects";
+export default Vue.extend({
+  name: "addProject",
+  data() {
+    return {
+      test: null,
+      project: {}
+    };
+  },
+  methods: {
+    ...mapActions(["addProject"]),
+    /**add the new project */
+    addNewProjec() {
+      console.log("this is the project that we want to add ", this.project);
+      this.addProject(this.project);
+    }
+  }
+});
 </script>
 
 <style>
