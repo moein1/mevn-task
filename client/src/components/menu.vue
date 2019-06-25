958732189cc7709daac821fb5bbb5aa36b550a86
mohammad <mohammad@mohammads-MacBook-Pro.local>
2019-06-26 07:41:25 +1000

    Add some content for showing the menu and we can start adding animation for showing them.

--- client/src/components/menu.vue
+++ client/src/components/menu.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>this is menu</div>
+  <div>
+    <ul class="main-menu">
+      <li class="menu-item" v-for="(menu , index) in 5" :key="index">
+        <span>{{'menu'+index}}</span>
+        <ul class="sub-menu">
+          <li
+            class="sub-menu-item"
+            v-for="(submenu , index2) in 5"
+            :key="index2"
+          >{{'submenu'+index2}}</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "menu",
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style>
