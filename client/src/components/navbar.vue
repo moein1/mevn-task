1b2495cbee5c606d01c7e93c53266320609407ab
mohammad <mohammad@mohammads-MacBook-Pro.local>
2019-07-16 18:28:05 +1000

    Change some of the style on the sidebar section.

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -1,37 +1,55 @@
 <template>
   <ul id="main-navbar">
     <li>
-      <router-link to="/">Home</router-link>
+      <router-link to="/">
+        <i class="fa fa-folder"></i>Home
+      </router-link>
     </li>
     <li>
-      <router-link to="/projects">Projects</router-link>
+      <router-link to="/projects">
+        <i class="fa fa-cube"></i>Projects
+      </router-link>
     </li>
     <li>
-      <router-link to="/card">Card</router-link>
+      <router-link to="/card">
+        <i class="fa fa-cube"></i>Card
+      </router-link>
     </li>
     <li>
-      <router-link to="grid">Grid</router-link>
+      <router-link to="grid">
+        <i class="fa fa-users"></i>Grid
+      </router-link>
     </li>
     <li>
-      <router-link to="portfolio">Portfolio</router-link>
+      <router-link to="portfolio">
+        <i class="fa fa-users"></i>Portfolio
+      </router-link>
     </li>
     <li>
-      <router-link to="eat">Eat</router-link>
+      <router-link to="eat">
+        <i class="fa fa-users"></i>Eat
+      </router-link>
     </li>
     <li>
-      <router-link to="netflix">Netflix</router-link>
+      <router-link to="netflix">
+        <i class="fa fa-users"></i>Netflix
+      </router-link>
+    </li>
+
+    <li>
+      <router-link to="modalContainer">
+        <i class="fa fa-users"></i>Modal container
+      </router-link>
     </li>
     <li>
-      <router-link to="/">Summart</router-link>
+      <router-link to="menu">
+        <i class="fa fa-users"></i>Animation menu
+      </router-link>
     </li>
     <li>
-      <router-link to="modalContainer">Modal container</router-link>
-    </li>
-    <li>
-      <router-link to="menu">Animation menu</router-link>
-    </li>
-    <li>
-      <router-link to="alert">Alert</router-link>
+      <router-link to="alert">
+        <i class="fa fa-users"></i>Alert
+      </router-link>
     </li>
   </ul>
 </template>
